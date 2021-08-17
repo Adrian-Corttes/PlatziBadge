d8abaa66f9185b8f7376a2ee58faf01c6d8a1185
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-17 13:44:07 -0500

    add styles

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import confLogo from '../img/badge-header.svg'
+import Gravatar from '../img/img-programador.png'
+import '../css/babge.css'
 
 const Badge = () => {
    return (
-      <div>
-         <img src="" alt="Logo Conferencia" />
+      <div className="Badge">
+         <div className="Badge__header">
+            <img src={confLogo}alt="Logo Conferencia" />
+         </div>
 
-         <div className="">
-            <img src="" alt="" />
+         <div className="Badge__section-name">
+            <img src={Gravatar} className="Badge__avatar" alt="avatar"/>
             <h1>Adrian <br/> Cortes</h1>
          </div>
 
-         <div className="">
+         <div className="Badge__section-info">
             <p>Web Developer Full Stack</p>
             <p>@Adrian_Corttes</p>
          </div>
 
-         <div className="">
+         <div className="Badge__footer">
             #platziconf
          </div>
       </div>
