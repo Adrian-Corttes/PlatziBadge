54f09722a765a1909869b4ba9a63314e3e696b62
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-17 13:54:58 -0500

    add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import Badge from './components/badge'
+import Badge from './components/badge';
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
