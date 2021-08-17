d92d98fff7dc0244a2cd0d6151179d670656183c
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-17 14:02:38 -0500

    modified index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Badge from './components/badge';
 import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
