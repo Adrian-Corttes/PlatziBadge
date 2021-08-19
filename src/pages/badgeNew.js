21e3916bf24ef2d79e5ccd9d9e15f1506559bc8c
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-18 22:50:16 -0500

    add src/pages/badgeNew.js

---
+++ src/pages/badgeNew.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import header from '../img/badge-header.svg'
+import Navbar from '../components/navbar'
+import Badge from '../components/badge';
+import Avatar from '../img/img-programador.png'
+import BabgeForm from '../components/BabgeForm'
+import '../pages/css/badgeNew.css'
+
+class BadgeNew extends React.Component{
+   state = {form: {}};
+
+   handleChange = e =>{
+      this.setState({
+         form: {
+            ...this.state.form,
+            [e.target.name] : e.target.value,
+         },
+      });
+   }
+
+  render(){
+   return (
+      <div>
+         <Navbar/>
+
+         <div className="BadgeNew__hero">
+            <img src={header}  className="img-fluid" alt=""/>
+         </div>
+
+         <div className="container">
+            <div className="row">
+               <div className="col-6">
+                  <Badge 
+                     firsName="Adrian"
+                     lastName="Cortes"
+                     jopTitle="Web Developer Full Stack"
+                     Twitter="Adrian_Corttes"
+                     avatar={Avatar}
+                  />
+               </div>
+
+               <div className="col-6">
+                  <BabgeForm 
+                     onChange={this.handleChange}
+                     formValues={this.state.form}
+                  />
+               </div>
+               
+            </div>
+         </div>
+      </div>
+   )
+  }
+}
+
+export default BadgeNew
