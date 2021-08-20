a32756283a69baca1ab0fb96806576e578510a12
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-19 21:46:32 -0500

    modified: src/index.js - src/pages/css/Badges.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import BadgeNew from './pages/badgeNew.js'
+//import BadgeNew from './pages/badgeNew.js'
+import Badges from './pages/Badges.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BadgeNew/>
+    <Badges/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,4 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
